22fc9cfcd0c946b8f1e26bf3bf2cede70eeadd6f
Jack Champagne <jackchampagne.r@gmail.com>
2020-09-06 12:31:23 -0400

    Add edit 1 to ch05-02 for tuple impl

--- ch05-02-rect-structs/src/main.rs
+++ ch05-02-rect-structs/src/main.rs
@@ -1,14 +1,18 @@
 // This is a project that shows how one could use structs in a program.
+// EDIT 0: To look at why this program is written the way it is, look
+// in the commit history for this file as it has been changed over time
+// to reflect the refacorings that have taken place to use structs.
+// EDIT 1: The same program has been implemented with tuples, but width
+// and height values are unnamed, which makes it confusing!
 fn main() {
-    let height = 15;
-    let width = 20;
+    let rect1 = (15, 20);
 
     println!(
         "The area of the rectangle is {} square pixels!",
-        area(height, width)
+        area(rect1)
     );
 }
 
-fn area(h: u32, w: u32) -> u32 {
-    h * w
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
